0c7367316fc861d656f849f7d156018bbd74d3a8
Dawid8209 <115348369+Dawid8209@users.noreply.github.com>
2024-03-23 12:48:13 +0100

    Add files via upload

---
+++ Project1/src/main/java/com/mycompany/project1/Project1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project1;
+
+/**
+ *
+ * @author student
+ */
+public class Project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, I'm DM!");
+    }
+}
